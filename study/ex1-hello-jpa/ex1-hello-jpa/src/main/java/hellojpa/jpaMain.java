41a78ddc4ec9a3eb74dccabfcdd212a37bf04e08
Inbeom Kim <dlsqja970210@naver.com>
2023-08-31 17:54:58 +0900

    study: JPA 2일차

--- study/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
+++ study/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
@@ -19,16 +19,27 @@ public class jpaMain {
 
         try {
 
-//            Member findMember = em.find(Member.class, 1L);
-            
-            // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
-//            findMember.setName("HelloJPA"); // JPA가 관리하기 때문에 변경됨
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            Member member2 = new Member();
+            member2.setUsername("B");
 
-            System.out.println("===============");
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("=================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("=================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
